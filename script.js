fc224eafe7e4f887ace7558a3898bec4f6d4ddd1
Bryan Gabriel de Bitencourt da Silveira <bryanconna2016@gmail.com>
2024-12-06 20:12:05 -0300

    Adicionado HTML, CSS e JS, além das imagens.

---
+++ script.js
@@ -0,0 +1,6 @@
+function calc() {
+    const calculadora = document.querySelector('.calc');
+    calculadora.style.height = '360px';
+    var botao = document.getElementById('botao')
+    botao.style.display = 'none'
+}
