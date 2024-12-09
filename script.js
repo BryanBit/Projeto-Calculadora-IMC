35edc3011c56267b99528b3e1422c600a0164cdd
Bryan Gabriel de Bitencourt da Silveira <bryanconna2016@gmail.com>
2024-12-09 13:24:57 -0300

    ,

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calc() {
     const calculadora = document.querySelector('.calc');
-    calculadora.style.height = '370px';
+    calculadora.style.height = '400px';
     var botao = document.getElementById('botao');
     botao.style.display = 'none';
 }
