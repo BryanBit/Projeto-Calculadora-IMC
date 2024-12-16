eba84cc94c2806da6a4c9e8193e0569b7a2a513b
Bryan Gabriel de Bitencourt da Silveira <bryanconna2016@gmail.com>
2024-12-16 13:24:54 -0300

    Criadas as funções em JavaScript.

--- calc-imc.js
+++
@@ -1,4 +0,0 @@
-function calcularIMC() {
-    var peso = document.getElementById ('ipeso.value');
-    var altura = document.getElementById ('ialtura.value');
-}
--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function mudarTamanho() {
     const calculadora = document.querySelector('.calc');
-    calculadora.style.height = '470px';
+    calculadora.style.height = '440px';
     var botao = document.getElementById('botao');
     botao.style.display = 'none';
-    var result = document.getElementById('result')
+    var botaoRefaz = document.getElementById ('botaorefaz')
+    botaoRefaz.style.display = 'block'
+    var result = document.getElementById ('result')
+    result.style.display = 'block'
     result.innerHTML = 'Seu IMC é'
-}
-function remake() {
-    const calculadora = document.querySelector('.calc');
-    calculadora.style.height = '350px';
 }
