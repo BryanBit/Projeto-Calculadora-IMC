e91a95c60d6f04e04763b4e3796eb9e2eca44046
Bryan Gabriel de Bitencourt da Silveira <bryanconna2016@gmail.com>
2024-12-29 17:11:47 -0300

    Criadas funções em JavaScript.

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-function mudarTamanho() {
-    const calculadora = document.querySelector('.calc');
-    calculadora.style.height = '440px';
-    var botao = document.getElementById('botao');
-    botao.style.display = 'none';
+function funcoes(){
+    validarCampos()
+    calcularImc()
+}
+
+function validarCampos() {
+    var peso = document.getElementById ('ipeso')
+    var alt = document.getElementById ('ialtura')
+
+    var pesoVal = Number (peso.value)
+    var altVal = Number (alt.value)
+
+    if (pesoVal == 0 && altVal == 0) {
+        alert ('Preencha corretamente todos os campos!')
+    } else if (pesoVal == 0) {
+        alert ('Preencha o campo PESO!')
+    } else if (altVal == 0) {
+        alert ('Preencha o campo ALTURA!')
+    }
+    else {
+        alterarDisplay()
+    }
+}
+function alterarDisplay() {
+    var calc = document.querySelector('.calc');
+    calc.style.height = '190px';
+    var botaoCalc = document.getElementById('botaoCalc');
+    botaoCalc.style.display = 'none';
     var botaoRefaz = document.getElementById ('botaorefaz')
     botaoRefaz.style.display = 'block'
     var result = document.getElementById ('result')
     result.style.display = 'block'
-    result.innerHTML = 'Seu IMC é'
+    var parag = document.getElementById ('parag')
+    parag.style.display = 'none'
+    var inputs = document.getElementById ('inputs')
+    inputs.style.display = 'none'
+}
+
+function calcularImc() {
+
 }
